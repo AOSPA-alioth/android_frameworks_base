10d60e390991fc2168a941daf316a2c70864e86e
BladeRunner-A2C <john.smith@unused.email>
2023-07-11 02:33:03 +0600

    PiHooks: Update redfin fingerprint
    Change-Id: I7bc7c45609966dc010b2e6fb320d8479036cdd68
    Signed-off-by: BladeRunner-A2C <john.smith@unused.email>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -74,7 +74,7 @@ public class PropImitationHooks {
         "DEVICE", "redfin",
         "PRODUCT", "redfin",
         "MODEL", "Pixel 5",
-        "FINGERPRINT", "google/redfin/redfin:13/TQ3A.230605.011/10161073:user/release-keys"
+        "FINGERPRINT", "google/redfin/redfin:13/TQ3A.230705.001/10216780:user/release-keys"
     );
 
     private static final Map<String, Object> sPixelXLProps = Map.of(
